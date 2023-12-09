6455b239bc2c0440282621e65cd5619c3f201a80
pegah tavakoli <pegah.tavakoliyaraki17@gmail.com>
2023-12-09 15:55:29 +0330

    initialize DeeKala project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./App.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const project = ReactDOM.createRoot(document.getElementById("root"));
+project.render(<App />);
