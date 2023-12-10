9abe070f58184e24887f6dc0fbb57546b9fa3a2f
pegah tavakoli <pegah.tavakoliyaraki17@gmail.com>
2023-12-10 15:42:19 +0330

    develop categories in main page

--- src/components/Main/Categories.jsx
+++ src/components/Main/Categories.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import '../../../css/main/categories.css'
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -12,16 +13,21 @@ import {
 } from "react-device-detect";
 export default function Categories() {
     const categories=[
-        {img:'./abzar.jpg',text:'ابزارآلات'},
-        {img:'./mashinalat.jpg',text:'ماشین‌آلات راه‌سازی'},
-        {img:'./art',text:'فرهنگ وهنر'},
-        {img:'./beauty',text:'زیبایی و بهداشت'},
-        {img:'./technology',text:'تکنولوژی'},
+        {img:'./abzar.jpg',text:'ابزارآلات',hasNewProduct:false,hasSeen:false},
+        {img:'./mashinalat.jpg',text:'ماشین‌آلات راه‌سازی',hasNewProduct:true,hasSeen:true},
+        {img:'./art.jpg',text:'فرهنگ وهنر',hasNewProduct:false,hasSeen:false},
+        {img:'./beauty.jpg',text:'زیبایی و بهداشت',hasNewProduct:false,hasSeen:false},
+        {img:'./technology.jpg',text:'تکنولوژی',hasNewProduct:true,hasSeen:false},
+        {img:'./abzar.jpg',text:'ابزارآلات',hasNewProduct:false,hasSeen:false},
+        {img:'./mashinalat.jpg',text:'ماشین‌آلات راه‌سازی',hasNewProduct:true,hasSeen:true},
+        {img:'./art.jpg',text:'فرهنگ وهنر',hasNewProduct:false,hasSeen:false},
+        {img:'./beauty.jpg',text:'زیبایی و بهداشت',hasNewProduct:true,hasSeen:false},
+        {img:'./technology.jpg',text:'تکنولوژی',hasNewProduct:false,hasSeen:false},
 ]
   const [swiperRef, setSwiperRef] = useState(null);
-  const view = isMobileOnly ? 3 : isTablet || isIPad13 ? 4: isDesktop?5:6;
+  const view = isMobileOnly ? 3 : isTablet || isIPad13 ? 4: 6;
   return (
-    <div className="d-lg-flex mySwiper align-items-center w-100 p-0">
+    <div className="d-lg-flex  align-items-center w-100 p-0 bg-white py-5">
       <Swiper
         onSwiper={setSwiperRef}
         slidesPerView={view}
@@ -35,12 +41,13 @@ export default function Categories() {
             return (
               <SwiperSlide
                 key={index}
-                className="col-3 "
                 style={{ verticalAlign: "middle" }}
               >
-           <div>
-            <img src={category.img} alt=""className="categoryImg" />
-            <span>{category.text}</span>
+           <div className="d-flex flex-column justify-content-center">
+           <div className={`${category.hasNewProduct && !category.hasSeen?'image-container':category.hasNewProduct && category.hasSeen?'viewed-image-container':''}`}>
+           <img src={category.img} alt=""className={`${category.hasNewProduct?'active-category':''} category-image`}/>
+           </div>
+            <span className="category-text mt-2">{category.text}</span>
            </div>
               </SwiperSlide>
             );
