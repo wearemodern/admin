a1bdddf2458db8aaaf444cbc15265947a0ac5daa
pegah tavakoli <pegah.tavakoliyaraki17@gmail.com>
2023-12-20 13:29:07 +0330

    making categories responsive

--- src/components/Main/Categories.jsx
+++ src/components/Main/Categories.jsx
@@ -71,38 +71,37 @@ export default function Categories() {
   ];
   return (
     <div className="d-lg-flex  align-items-center w-100 p-0 bg-white py-5">
-    
-        <div className="d-flex w-100 justify-content-center   flex-wrap align-items-center">
-          {categories.map((category, index) => {
-            return (
-              <div key={index} style={{ verticalAlign: "middle" }} className="col-2 d-flex flex-column justify-content-center " >
-             
-                  {/* category img */}
-                  <div
-                    className={`${
-                      category.hasNewProduct && !category.hasSeen
-                        ? "image-container "
-                        : category.hasNewProduct && category.hasSeen
-                        ? "viewed-image-container"
-                        : ""
-                    } d-flex flex-column justify-content-center align-items-center text-center mx-auto` }
-                  >
-                    <img
-                      src={category.img}
-                      alt=""
-                      className={`${
-                        category.hasNewProduct ? "active-category" : ""
-                      } category-image`}
-                    />
-                  </div>
-                  {/*category name  */}
-                  <span className="category-text mt-2 text-center">{category.text}</span>
-                </div>
-              
-            );
-          })}
-        </div>
-     
+      <div className="d-flex w-100 justify-content-center   flex-wrap align-items-center">
+        {categories.map((category, index) => {
+          return (
+            <div
+              key={index}
+              style={{ verticalAlign: "middle" }}
+              className="col-4 col-sm-2 mt-3 d-flex flex-column justify-content-center "
+            >
+              {/* category img */}
+              <div
+                className={`${
+                  category.hasNewProduct && !category.hasSeen
+                    ? "image-container"
+                    : "viewed-image-container "
+                } d-flex flex-column justify-content-center align-items-center text-center mx-auto`}
+              >
+                <img
+                  src={category.img}
+                  alt="category-img"
+                  className=" category-image active-category"
+                  
+                />
+              </div>
+              {/*category name  */}
+              <span className="category-text mt-2 text-center">
+                {category.text}
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
