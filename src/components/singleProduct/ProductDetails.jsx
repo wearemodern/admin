013de2c72b8898b81db0ebb6a99052077c843dcd
pegah tavakoli <pegah.tavakoliyaraki17@gmail.com>
2024-01-03 14:41:00 +0330

    develop product details component (10%)

---
+++ src/components/singleProduct/ProductDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './../../../css/singleProduct/singleProduct.css'
+export default function ProductDetails() {
+  return (
+    <div className='productDetails-box w-100 '>
+<img src="./art.jpg" alt="" className='productDetails-mainPic col-4'/>
+
+    </div>
+  )
+}
