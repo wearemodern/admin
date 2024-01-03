71081fcf18339bb47bc17c4c79f3c710ff30f6a4
pegah tavakoli <pegah.tavakoliyaraki17@gmail.com>
2024-01-03 16:22:45 +0330

    develop product details component(20%)

--- src/components/singleProduct/ProductDetails.jsx
+++ src/components/singleProduct/ProductDetails.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import "./../../../css/singleProduct/singleProduct.css";
 export default function ProductDetails() {
   return (
-    <div className="productDetails-box w-100 ">
+    <div className="productDetails-box w-100 mt-3">
       <div className="col-4">
-        <img src="./art.jpg" alt="" className="productDetails-mainPic" />
+        <img src="/beauty.jpg" alt="" className="productDetails-mainPic" />
+      </div>
+      <div className="col-4 productDetails border border-grey ">
+
       </div>
     </div>
   );
